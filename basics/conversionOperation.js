0c102dfd7cfd065d3fa0145b917d4aa70edc56dd
Vishal_Shimpi <139038615+VSShimpi@users.noreply.github.com>
2024-07-01 07:03:37 +0000

    Conversion of operation

---
+++ basics/conversionOperation.js
@@ -0,0 +1,61 @@
+let score = false;
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+
+let isloggedIn = "";
+
+let valueInBoolean = Boolean(isloggedIn);
+// console.log(valueInBoolean);
+// console.log(typeof valueInBoolean);
+
+// 1 => true, 0 => false
+// "" => false
+// "Vishal" => true
+
+let someNumber = 33;
+
+let valueInString = String(someNumber);
+// console.log(valueInString);
+// console.log(typeof valueInString);
+
+///*************** Operation *************///
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Vishal";
+let str2 = " Shimpi";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
