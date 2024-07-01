2afc8990473e1aa4e0a781419c6210bc87430b68
Vishal_Shimpi <139038615+VSShimpi@users.noreply.github.com>
2024-07-01 06:02:37 +0000

    conversion of Operation

---
+++ basics/conversionOpertation.js
@@ -0,0 +1,25 @@
+let score = false;
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+
+let isloggedIn = "";
+
+let valueInBoolean = Boolean(isloggedIn);
+// console.log(valueInBoolean);
+// console.log(typeof valueInBoolean);
+
+// 1 => true, 0 => false
+// "" => false
+// "Vishal" => true
+
+let someNumber = 33;
+
+let valueInString = String(someNumber);
+// console.log(valueInString);
+// console.log(typeof valueInString);
