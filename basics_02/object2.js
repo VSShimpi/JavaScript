daaf8a7b5129612f09fe8b9ca31ae4e61e006b9d
Vishal_Shimpi <139038615+VSShimpi@users.noreply.github.com>
2024-07-04 09:53:43 +0000

    Object in JS

---
+++ basics_02/object2.js
@@ -0,0 +1,24 @@
+const course = {
+    coursename : "Js in hindi",
+    price : "999",
+    courseInstructor : "hitesh", 
+}
+
+// Object De-Construction 
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor)
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "price": "free",
+//     "coursename": "Js in hindi"
+// }
+
+// [
+//     {}
+//     {}
+//     {}
+// ]
